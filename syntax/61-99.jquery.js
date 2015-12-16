868890972b6997a8e6af3dfc32a9ff666249835a
VeraKH <vera_khatskevich@sibmail.com>
2015-12-16 22:26:48 +0300

    full js repo commit

---
+++ syntax/61-99.jquery.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+//1. Change text value of the element
+
+// var HR = $(".user-full-name").text("Vera Swlovworld");
+
+//2. Show parameters of the element
+
+// function whCalc(element){
+//      var	calcelem = "."+element;
+//      var wElem = $(calcelem).width();
+//       var hElem = $(calcelem).height();
+
+//       $(alert("Width: "+wElem + ". Height: "+ hElem));
+// }
+// whCalc("site-right-area");
+
+//3. Hide and show elements
+
+// var userpix = $("#left-column-block");
+// userpix.hide().show(4000);
+
+// var footer = $(".footer");
+// footer.hide().show(4000);
+
+// var tabsContents = $(".site-right-area");
+// tabsContents.hide().show(4000);
+
+// var divParam = $(".site-right-area").width(600).height(800);
+// alert(divParam);
+
+//4. Change the html code for the elements
+
+//$(".footer").html("<ul><li><a>Explore</a></li><li><a>Start course</a></li><li><a>Blog</a></li></ul>");
+
+//5. FadeOut and fadeIn method
+
+// var userpix = $("#left-column-block");
+// userpix.fadeOut("slow");
+
+// var userpix = $("#left-column-block");
+// userpix.hide().fadeIn(4000);
+
+// var footer = $(".footer");
+// footer.hide().fadeIn(4000);
+
+// var tabsContents = $(".site-right-area");
+// tabsContents.hide().fadeIn(4000);
+
+}); 
